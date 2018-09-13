ac764c194e70caf7ee2f925a8b5a744b5944ca67
saintnamee <saintnamee@hotmail.com>
2018-09-13 12:45:00 +0700

    chickpoint 5

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -10,9 +10,16 @@ export default class CharacterCard extends Component {
 
     activate = () => {
         if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
             this.setState({ active: true })
         }
     }
+    componentDidUpdate(prevProps) {
+        if (prevProps.attempt != this.props.attempt) {
+            this.setState({ active: false })
+        }
+    }
+
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
