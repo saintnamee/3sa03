b0f7ca3aed9426f636b47724b421c546b1533e25
saintnamee <saintnamee@hotmail.com>
2018-09-13 10:36:25 +0700

    sad

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">saint</h1>
-        </header>
-        <p className="App-intro">
-          To get staouijuirted, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CharacterCard value="h"/>
+        <CharacterCard value="i"/>
+
       </div>
     );
   }
